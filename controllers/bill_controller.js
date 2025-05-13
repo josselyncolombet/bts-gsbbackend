8810e847d9fb15ff47486ba4198bde2917354957
josselyncolombet <josselyn.colombet@laposte.net>
2025-05-13 14:46:44 +0200

    update

--- controllers/bill_controller.js
+++ controllers/bill_controller.js
@@ -1,16 +1,35 @@
 const Bill = require('../models/bill_model')
+const { uploadToS3 } = require('../utils/s3')
 
 const createBill = async (req, res) => {
     try {
-        const { date, amount, proof, description, status, type } = req.body
+        const { date, amount, description, status, type } = JSON.parse(req.body.metadata)
+        console.log(date, amount, description, status, type)
         const { id } = req.user
-        const bill = new Bill({ date, amount, proof, description, status, type, user: id })
+        
+        // Handle file upload
+        let proofUrl
+        if (req.file) {
+            proofUrl = await uploadToS3(req.file)
+        } else {
+            throw new Error('Proof image is required', { cause: 400 })
+        }
+        const bill = new Bill({ 
+            date: date, 
+            amount: amount, 
+            proof: proofUrl, 
+            description: description, 
+            status: status, 
+            type: type, 
+            user: id 
+        })
         await bill.save()
         res.status(201).json(bill)
     } catch (error) { 
         if (error['cause'] === 400) {
             res.status(400).json({ message: error.message })
         } else {
+            console.error('Error creating bill:', error)
             res.status(500).json({ message: "Server error" })
         }
     }
